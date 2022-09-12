d6575246d8589f7d3630a0d12c919040814122f7
Pablo <jafman3@gmail.com>
2022-09-12 14:09:02 -0400

    pushed fix for talent()

--- main.py
+++ main.py
@@ -834,9 +834,9 @@ def talent():
 
         project_keywords= []
         for item in user_projects:
-            for key in item["data"]["project"]["keywords"]:
-                project_keywords.append(key["keyword"])
             try:
+                for key in item["data"]["project"]["keywords"]:
+                    project_keywords.append(key["keyword"])
                 matchedSkill_docs = myLib.getMatches_byList("skill_match_index", project_keywords)
                 matchedProject_id = item["ref"].id()
                 matchedProject_title = item["data"]["project"]["title"]
